609a00faf75f91df41b7ab420eb05ee9ead95563
Yujin Lee <uujinlee.dev@gmail.com>
2023-08-08 21:45:25 +0900

    update files

---
+++ algorithm study/BOJ/temp/10994.cpp
@@ -0,0 +1,19 @@
+// boj 10994 별 찍기 - 19
+// recursion
+#include <iostream>
+using namespace std;
+
+int N; //1 ≤ N ≤ 100
+
+void star(int n) {
+    
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    star(N);
+    return 0;
+}
---
+++ algorithm study/BOJ/temp/18511.cpp
@@ -0,0 +1,29 @@
+// // boj 18511 큰 수 구성하기
+// // brute-force 말고 dfs??
+// #include <algorithm>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int N, K;  // 10 ≤ N ≤ 100,000,000, 1 ≤ K의 원소의 개수 ≤ 3
+// vector<int> v;
+// vector<int> numv;
+// int find() {}
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+
+//     cin >> N >> K;
+//     for (int i = 0; i < K; i++) {
+//         int temp;
+//         cin >> temp;
+//         v.push_back(temp);
+//     }
+//     sort(v.begin(), v.end());
+//     reverse(v.begin(), v.end());
+//     cout << find();
+
+//     return 0;
+// }
