57bbc7b61a47aca7cf44febfa38f554316a5226e
Yujin <uujin.dev@gmail.com>
2024-02-01 14:24:51 +0900

    update readme

--- README.md
+++ README.md
@@ -12,4 +12,6 @@ Today I Learned
 
     💗tistory https://lavendergray.tistory.com/
 
+    💗velog https://velog.io/@uujinlee/posts
+
     💗notion https://www.notion.so/lavendergray/CS-study-f1af0a6be5b143be8eaa96d24478e1bb?pvs=4
--- algorithm study/BOJ/2. Silver/11660.js
+++ algorithm study/BOJ/2. Silver/11660.js
@@ -11,18 +11,19 @@ let arr = input.slice(1, n + 1).map((str) => str.split(" ").map(Number));
 let temp = Array.from(Array(n + 1), () => new Array(n + 1).fill(0));
 
 for (let i = 1; i <= n; i++) temp[i][1] = arr[i - 1][0];
-//행별 누적합이 아닌 좌표별 누적합
+//행별 누적합이 아닌3 좌표별 누적합
 for (let i = 1; i < n + 1; i++) {
   for (let j = 1; j < n + 1; j++) {
     temp[i][j] = arr[i - 1][j - 1] + temp[i][j - 1] + temp[i - 1][j] - temp[i - 1][j - 1];
   }
 }
 
+let sum = [];
 for (let i = 0; i < m; i++) {
-  let sum = 0;
   const [x1, y1, x2, y2] = input[n + i + 1].split(" ").map(Number);
 
   //겹치는 부분 빼기, 두 번 뺀 부분 더하기
-  sum = temp[x2][y2] - temp[x1 - 1][y2] - temp[x2][y1 - 1] + temp[x1 - 1][y1 - 1];
-  console.log(sum);
+  sum[i] = temp[x2][y2] - temp[x1 - 1][y2] - temp[x2][y1 - 1] + temp[x1 - 1][y1 - 1];
 }
+
+for (let i = 0; i < m; i++) console.log(sum[i]);
