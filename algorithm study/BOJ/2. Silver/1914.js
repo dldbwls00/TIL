70337276ee1244b143702fe9fc09e482ddfa04ce
Yujin <uujin.dev@gmail.com>
2024-02-04 15:10:14 +0900

    upload files

--- algorithm study/BOJ/2. Silver/1914.js
+++ algorithm study/BOJ/2. Silver/1914.js
@@ -1,8 +1,24 @@
 // boj 1914 하노이 탑
 // recursion
+
 const input = require("fs").readFileSync("./example.txt").toString().split("\n");
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 const n = input[0].split(" ").map(Number); //1~100
 
-let k = 0; //옮긴 횟수
+function hanoi(n, start, via, end) {
+  if (n < 1) return;
+  if (n == 1) {
+    console.log(start, end);
+    return;
+  }
+  hanoi(n - 1, start, end, via); //n-1개를 장대1 -> 장대2
+  console.log(start, end);
+  hanoi(n - 1, via, start, end); //n-1개를 장대2 -> 장대3
+
+  return;
+}
+
+console.log(2 ** n - 1);
+if (n > 20) return;
+hanoi(n, 1, 2, 3);
