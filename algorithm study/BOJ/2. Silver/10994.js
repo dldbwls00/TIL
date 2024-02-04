876c68a77d197dc5f8b34ad224f69cc081a9072d
Yujin <uujin.dev@gmail.com>
2024-02-05 06:20:52 +0900

    upload files

--- algorithm study/BOJ/2. Silver/10994.js
+++ algorithm study/BOJ/2. Silver/10994.js
@@ -3,5 +3,34 @@
 
 const input = require("fs").readFileSync("./example.txt").toString().split("\n");
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
-
 const n = input[0].split(" ").map(Number);
+
+const maximum = (n - 1) * 4 + 1; //가장 바깥 테두리 길이: 99*4+1=397
+let map = Array.from(Array(maximum), () => Array(maximum).fill(" "));
+
+function star(n, xy) {
+  const len = (n - 1) * 4 + 1;
+  if (n === 0) return;
+  if (n === 1) {
+    map[xy][xy] = "*";
+    return;
+  }
+  for (let i = xy; i < xy + len; i++) {
+    //가로
+    map[i][xy] = "*";
+    map[i][xy + len - 1] = "*";
+
+    //세로
+    map[xy][i] = "*";
+    map[xy + len - 1][i] = "*";
+  }
+  return star(n - 1, xy + 2);
+}
+star(n, 0);
+
+let result = "";
+for (let i = 0; i < maximum; i++) {
+  for (let j = 0; j < maximum; j++) result += map[i][j];
+  result += "\n";
+}
+console.log(result);
--- algorithm study/BOJ/2. Silver/2877.js
+++ algorithm study/BOJ/2. Silver/2877.js
@@ -1,3 +1,4 @@
+// 참고: https://velog.io/@hygge/Python-%EB%B0%B1%EC%A4%80-2877-4%EC%99%80-7-%EA%B5%AC%ED%98%84
 // boj 2877 4와 7
 // implementation
 
@@ -6,13 +7,32 @@ const input = require("fs").readFileSync("./example.txt").toString().split("\n")
 
 const k = input[0].split(" ").map(Number);
 
-function digit(k) {
-  let n = 0;
-  let sum = 0;
-  for (; k >= sum; n++) sum += 2 ** n;
-  //   console.log(n);
-  //   console.log(sum);
-  return n - 1;
-}
+// function digitCalc(k) {
+//   let n = 0;
+//   let sum = 0;
+//   for (; k >= sum; n++) sum += 2 ** n;
+//   return n - 1;
+// }
+// const digit = digitCalc(k);
+// let result = [];
+// for (let i = 0; i < digit; i++) result.push(4); //initialization
 
-digit(k);
+// let sum = 0;
+// for (let i = 1; i < digit; i++) sum += 2 ** i;
+// let left = k - sum; //얼마나 더해야하는지
+
+// left--;
+// for (let i = result.length - 1; i >= 0; i--) {
+//   if (left % 2 != 0) result[i] = 7; //나누기로 뒷자리부터 더함
+//   if (left < 2) break;
+//   left /= 2;
+// }
+
+// let ans = "";
+// for (let i = 0; i < result.length; i++) ans += result[i];
+// console.log(ans);
+
+let result = (parseInt(k) + 1).toString(2);
+result = result.replace(/0/g, "4").replace(/1/g, "7");
+result = result.slice(1); //맨 앞자리
+console.log(result);
