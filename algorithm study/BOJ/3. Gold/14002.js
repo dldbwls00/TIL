0c035b92f66defae97030b303bca37e3fc05acb4
Yujin <uujin.dev@gmail.com>
2024-03-18 13:57:29 +0900

    upload files

--- algorithm study/BOJ/3. Gold/14002.js
+++ algorithm study/BOJ/3. Gold/14002.js
@@ -1,45 +1,71 @@
 // boj 14002 가장 긴 증가하는 부분 수열 4
 // tree traversal, two pointer, LIS
+// LIS
 // dp: 비슷한 문제: https://www.acmicpc.net/problem/11722
-
+// 풀이참고: https://velog.io/@arthur/14002.-%EA%B0%80%EC%9E%A5-%EA%B8%B4-%EC%A6%9D%EA%B0%80%ED%95%98%EB%8A%94-%EB%B6%80%EB%B6%84-%EC%88%98%EC%97%B4-4-node.js-javascript
 const input = require("fs").readFileSync("./example.txt").toString().split("\n");
 // const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
-const n = parseInt(input[0]);
-const arr = input[1].split(" ").map(Number);
+// const n = parseInt(input[0]);
+// const arr = input[1].split(" ").map(Number);
+// arr.splice(0, 0, 0);
 
-arr.splice(0, 0, 0);
-let dp = new Array(n + 1).fill(0); //인덱스까지의 최대 길이
+//틀렸습니다 이유 모르겠음...
+// let dp = new Array(n + 1).fill(0); //인덱스까지의 최대 길이
+// for (let i = 1; i <= n; i++) {
+//   dp[i] = 1;
+//   for (let j = 1; j <= i; j++) {
+//     // 현재 인덱스 i보다 작은 인덱스 값들과 비교
+//     // 1. 현재 인덱스의 값보다 큰지
+//     // 2. 해당 큰 값을 선택했을 때 최대 길이가 될 수 있는지
+//     if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+//   }
+// }
 
-for (let i = 1; i <= n; i++) {
-  dp[i] = 1;
-  for (let j = 1; j <= i; j++) {
-    // 현재 인덱스 i보다 작은 인덱스 값들과 비교
-    // 1. 현재 인덱스의 값보다 큰지
-    // 2. 해당 큰 값을 선택했을 때 최대 길이가 될 수 있는지
-    if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
-  }
-}
+// // 반례: 1/1 입력시 0
+// // 반례: 4/10 30 20 30 입력시 3/10 20 30
+// if (n === 1) {
+//   console.log(0);
+//   return;
+// } else {
+//   let max = -1;
+//   for (let i = 1; i <= n; i++) max = Math.max(max, dp[i]);
+//   console.log(max);
 
-// 반례: 1/1 입력시 0
-// 반례: 4/10 30 20 30 입력시 3/10 20 30
-if (n === 1) {
-  console.log(0);
-  return;
-} else {
-  let max = -1;
-  for (let i = 1; i <= n; i++) max = Math.max(max, dp[i]);
-  console.log(max);
+//   let temp = [];
+//   for (let i = n; i >= 1, max >= 1; i--) {
+//     if (dp[i] === max) {
+//       temp.push(arr[i]);
+//       max--;
+//     }
+//   }
+//   let result = "";
+//   for (let i = temp.length - 1; i >= 0; i--) result += temp[i] + " ";
+//   console.log(result);
+//   // console.log(dp);
+// }
 
-  let temp = [];
-  for (let i = n; i >= 1, max >= 1; i--) {
-    if (dp[i] === max) {
-      temp.push(arr[i]);
-      max--;
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+
+let dp = Array.from({ length: n }, () => 0);
+let temp = [];
+
+for (let i = 0; i < n; i++) {
+  let maximum = 0;
+  let maxidx = -1;
+
+  for (let j = 0; j < i; j++) {
+    if (arr[i] > arr[j] && dp[j] > maximum) {
+      maximum = dp[j];
+      maxidx = j;
     }
   }
-  let result = "";
-  for (let i = temp.length - 1; i >= 0; i--) result += temp[i] + " ";
-  console.log(result);
-  console.log(dp);
+  dp[i] = maximum + 1;
+  temp[i] = maxidx !== -1 ? temp[maxidx].concat(arr[i]) : [arr[i]];
 }
+
+let answer = Math.max(...dp);
+
+console.log(answer);
+console.log(temp[dp.indexOf(answer)].join(" "));
--- algorithm study/BOJ/3. Gold/2263.js
+++ algorithm study/BOJ/3. Gold/2263.js
@@ -11,9 +11,9 @@ const n = parseInt(input[0]);
 const inorderarr = input[1].split(" ").map(Number);
 const postorderarr = input[2].split(" ").map(Number);
 
-// console.log("n: " + n);
-// console.log("inorderarr: " + inorderarr);
-// console.log("postorderarr: " + postorderarr);
+console.log("n: " + n);
+console.log("inorderarr: " + inorderarr);
+console.log("postorderarr: " + postorderarr);
 
 //트리는 n개의 정점을 갖고, 1부터 n까지 중복 없이 번호를 가짐
 //이진탐색트리가 아님: 노드의 크기 조건 없이 inorder와 postorder만 주어짐
@@ -22,44 +22,66 @@ const postorderarr = input[2].split(" ").map(Number);
 //나눈 서브트리에서도 맨 끝 값이 그 서브트리의 root
 //이 root를 기준으로 inorder를 이용해 left, right로 나눔
 
-let tree = {};
+//메모리 초과
+// let tree = {};
+// let ans = "";
+
+// function maketree(node, postarr, inarr) {
+//   if (node === ".") return;
+//   if (postarr.length === 0) return;
+
+//   tree[node] = [];
+//   let inIdx = inarr.indexOf(node);
+//   let postLeft = [];
+//   let postRight = [];
+//   let inLeft = [];
+//   let inRight = [];
+
+//   for (let i = 0; i < inIdx; i++) {
+//     postLeft.push(postarr[i]);
+//     inLeft.push(inarr[i]);
+//   }
+//   for (let i = inIdx; i < postarr.length - 1; i++) postRight.push(postarr[i]);
+//   for (let i = inIdx + 1; i < postarr.length; i++) inRight.push(inarr[i]);
+
+//   if (postLeft.length !== 0) tree[node][0] = postLeft[postLeft.length - 1];
+//   else tree[node][0] = ".";
+//   if (postRight.length !== 0) tree[node][1] = postRight[postRight.length - 1];
+//   else tree[node][1] = ".";
+
+//   maketree(postLeft[postLeft.length - 1], postLeft, inLeft);
+//   maketree(postRight[postRight.length - 1], postRight, inRight);
+// }
+
+// function preorder(node) {
+//   if (node == ".") return;
+
+//   ans += node + " ";
+//   preorder(tree[node][0]);
+//   preorder(tree[node][1]);
+// }
+// maketree(postorderarr[n - 1], postorderarr, inorderarr);
+// preorder(postorderarr[n - 1]);
+// console.log(ans.substring(0, ans.length - 1));
+
+//트리를 만들고 그걸 다시 preorder로 프린트 하는 게 아니라
+//postorder의 끝 값은 항상 root이므로 이 root를 계속 출력하는 방식으로
 let ans = "";
+function preorder(instart, inend, poststart, postend) {
+  if (instart > inend || poststart > postend) return;
+  let rootIdx = inorderarr.indexOf(postorderarr[postend]);
+  let leftlength = rootIdx - instart;
+  let rightlength = inend - rootIdx;
+  ans += inorderarr[rootIdx] + " ";
 
-function maketree(node, postarr, inarr) {
-  if (node === ".") return;
-  if (postarr.length === 0) return;
+  console.log("rootIdx: " + rootIdx);
+  console.log("leftlength: " + leftlength);
+  console.log("rightlength:" + rightlength);
+  console.log("inorderarr[rootIdx]: " + inorderarr[rootIdx]);
 
-  tree[node] = [];
-  let inIdx = inarr.indexOf(node);
-  let postLeft = [];
-  let postRight = [];
-  let inLeft = [];
-  let inRight = [];
-
-  for (let i = 0; i < inIdx; i++) {
-    postLeft.push(postarr[i]);
-    inLeft.push(inarr[i]);
-  }
-  for (let i = inIdx; i < postarr.length - 1; i++) postRight.push(postarr[i]);
-  for (let i = inIdx + 1; i < postarr.length; i++) inRight.push(inarr[i]);
-
-  if (postLeft.length !== 0) tree[node][0] = postLeft[postLeft.length - 1];
-  else tree[node][0] = ".";
-  if (postRight.length !== 0) tree[node][1] = postRight[postRight.length - 1];
-  else tree[node][1] = ".";
-
-  maketree(postLeft[postLeft.length - 1], postLeft, inLeft);
-  maketree(postRight[postRight.length - 1], postRight, inRight);
+  preorder(instart, rootIdx - 1, poststart, poststart + leftlength - 1);
+  preorder(rootIdx + 1, inend, poststart + leftlength, postend - 1);
 }
 
-function preorder(node) {
-  if (node == ".") return;
-
-  ans += node + " ";
-  preorder(tree[node][0]);
-  preorder(tree[node][1]);
-}
-
-maketree(postorderarr[n - 1], postorderarr, inorderarr);
-preorder(postorderarr[n - 1]);
-console.log(ans.substring(0, ans.length - 1));
+preorder(0, n - 1, 0, n - 1);
+console.log(ans);
